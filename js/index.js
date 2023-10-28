f14f5b638b34f8f360c3ebfc703c338395f219f9
ahmedsaifsobraj <ahmedsaifsobraj@gmail.com>
2023-10-29 00:31:57 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -4,5 +4,25 @@ const getPhones = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchElementText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayPhones(data.data))
+}
+const displayPhones = (datas) => {
+
+    const container = document.getElementById('phones-container');
+    container.textContent = '';
+    for (const data of datas) {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        console.log(data.image);
+        div.innerHTML = `
+        <div class="card">
+        <img src="${data.image}" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">${data.brand}</h5>
+            
+            <p class="card-text">${data.phone_name}</p>
+        </div>
+        </div>`;
+        container.appendChild(div);
+    }
 }
